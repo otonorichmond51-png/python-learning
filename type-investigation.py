59aff620d7d44c8ba2385fa92602177e2859757b
otonorichmond51-png <otonorichmond51@gmail.com>
2026-08-17 00:28:16 +0100

    investigation

---
+++ type-investigation.py
@@ -0,0 +1,20 @@
+a = 10
+b = 10.5
+c = "10"
+d = True
+e = None
+print(a)
+print(type(a))
+print(id(a))
+print(b)
+print(type(b))
+print(id(b))
+print(c)
+print(type(c))
+print(id(c))
+print(d)
+print(type(d))
+print(id(d))
+print(e)
+print(type(e))
+print(id(e))
