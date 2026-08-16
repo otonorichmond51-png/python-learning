60f74b89a6f2a44a2d3cd1e320f52c12fbc1084a
otonorichmond51-png <otonorichmond51@gmail.com>
2026-08-16 23:37:05 +0100

    info

---
+++ personal-info.py
@@ -0,0 +1,13 @@
+name = "Richmond"
+age = 19
+country = "Nigeria"
+city = "Abuja"
+profession = "Computer Science Student"
+is_student = True
+
+print(name)
+print(age)
+print(country)
+print(city)
+print(profession)
+print(is_student)   
