c86670d1ad7f400f4193cf50addc05de353658e3
otonorichmond51-png <otonorichmond51@gmail.com>
2026-08-17 00:51:20 +0100

    mini project 1

---
+++ user-file.py
@@ -0,0 +1,28 @@
+name = "Richmond"
+age = 19
+country = "Nigeria"
+profession = "computer science student"
+experience = "undergoing"
+is_employed = False
+account_balance = 50000.0
+print("========================")
+print("       USER PROFILE  ")
+print("========================")
+print("Name:", name)
+print("Age:", age)
+print("Country:", country)
+print("Profession:", profession)
+print("Experience:", experience)
+print("Employed:", is_employed)
+print("Balance:", account_balance)
+
+print("========================")
+
+print("\nTypes of variables:")
+print("name:", type(name))
+print("age:", type(age))
+print("country:", type(country))
+print("profession:", type(profession))
+print("experience:", type(experience))
+print("is_employed:", type(is_employed))
+print("account_balance:", type(account_balance))
