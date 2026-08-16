a119f55abe26b804ecd7fa527ad1dea83dfa8882
otonorichmond51-png <otonorichmond51@gmail.com>
2026-08-16 23:43:02 +0100

    data type

---
+++ type.py
@@ -0,0 +1,13 @@
+name = "Richmond"
+age = 19
+country = "Nigeria"
+city = "Abuja"
+profession = "Computer Science Student"
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(country))
+print(type(city))
+print(type(profession))
+print(type(is_student))   
