d2fe66e8e8f9ec28618ecaf215ea5f660a843488
otonorichmond51-png <otonorichmond51@gmail.com>
2026-08-18 22:32:01 +0100

    simple arithmetic calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+first_num = float(input("Enter the first number: "))
+second_num = float(input("Enter the second number: "))
+addition = first_num + second_num
+subtraction = first_num - second_num
+multiplication = first_num * second_num
+division = first_num / second_num
+print()
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
