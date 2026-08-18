cbfb579f01ba219b7b53153c33205841d1f5e440
otonorichmond51-png <otonorichmond51@gmail.com>
2026-08-18 21:38:17 +0100

    delete age-caculator

--- age-caculator.py
+++
@@ -1,4 +0,0 @@
-birth_year = int(input("Enter your birth year: "))
-current_year = int(input("Enter the current year: "))
-age = current_year - birth_year
-print("You are approximately", age, "years old.")
