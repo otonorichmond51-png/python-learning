afcac06992382425695f3890a0f552fcf3371ead
otonorichmond51-png <otonorichmond51@gmail.com>
2026-08-21 02:57:30 +0100

    python operators

---
+++ 2nd-calculator.py
@@ -0,0 +1,17 @@
+first_number = float(input("Enter first number:"))
+second_number = float(input("Enter second number:"))
+addition = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+floor_division = first_number // second_number
+remainder = first_number % second_number
+power = first_number ** second_number
+print()
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Floor Division:", floor_division)
+print("Remainder:", remainder)
+print("Power:", power)
