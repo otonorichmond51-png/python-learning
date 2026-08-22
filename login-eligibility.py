0d636d31fedd9b4d00d69df862aac1d47422b795
otonorichmond51-png <otonorichmond51@gmail.com>
2026-08-22 23:52:30 +0100

    remove login requirement

--- login-eligibility.py
+++
@@ -1,6 +0,0 @@
-age = int(input("Enter your age:"))
-has_id = input("Do you have an id?")
-if age >= 18 and has_id=="yes":
-    print("You meet both requirements")
-else:
-    print("You do not meet both requirements")
