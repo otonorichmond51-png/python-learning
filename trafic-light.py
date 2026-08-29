77b216c760ea6a24cf405fea4fcb6cf6be694e6c
otonorichmond51-png <otonorichmond51@gmail.com>
2026-08-29 01:52:32 +0100

    traffic light colors

---
+++ trafic-light.py
@@ -0,0 +1,10 @@
+light = input("Enter traffic light: ").lower()
+if light == "red":
+    print("Stop")
+elif light == "yellow":
+    print("Get ready")
+elif light == "green":
+    print("Go")
+else:
+    print("Invalid traffic light")
+
