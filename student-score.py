ba058e29cf6b1bb1a18f3176668007129258c09f
otonorichmond51-png <otonorichmond51@gmail.com>
2026-09-18 22:56:13 +0100

    std scores

---
+++ student-score.py
@@ -0,0 +1,18 @@
+scores = [75, 82, 91, 68, 88]
+
+#Print all scores.
+print("Scores: ", scores)
+
+#Sort them.
+scores.sort()
+print("Sorted: ", scores)
+
+#Print the highest score.
+print("Highest score: ", max(scores))
+
+#Print the lowest score.
+print("Lowest score: ", min(scores))
+
+#Calculate the average.
+average = sum(scores)/len(scores)
+print("Average: ", average)
